1c7b21b20e372106bf332806ee8a8fa95e7ed583
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2024-12-04 11:01:37 -0600

    Added comments.

--- aplic-test.cpp
+++ aplic-test.cpp
@@ -89,10 +89,10 @@ main(int, char**)
   child3->write(child3->csrAddress(CsrNumber::Domaincfg), sizeof(CsrValue), dcfg.value_);
 
   // Define an interrupt delivery callback
-  auto callback = [] (unsigned hartIx, bool machine, bool ip) -> bool {
-    std::cerr << "Interrupt at hart " << hartIx << " privilege "
-	      << (machine? "machine" : "supervisor")
-	      << " interrupt " << ip << '\n';
+  auto callback = [] (unsigned hartIx, bool mPrivelege, bool interState) -> bool {
+    std::cerr << "Delivering interrup hart=" << hartIx << " privilege="
+	      << (mPrivelege? "machine" : "supervisor")
+	      << " interrupt-state=" << (interState? "on" : "off") << '\n';
     return true;
   };
   aplic.setDeliveryMethod(callback);
@@ -106,82 +106,117 @@ main(int, char**)
   aplic.setImsicMethod(imsicFunc);
 
 
-  // Enable inetrrupt for source 1.
-  unsigned writeSize = 4, sourceIx = 1, hart = 0, threshold = 2;
-  child->write(child->csrAddress(CsrNumber::Setienum), writeSize, sourceIx);
+  // Configuration done. Testing APIs.
+  unsigned hart = 0;
 
-  // Make source1 target hart 0 with priority 1.
+  // 1. Enable inetrrupt for source 1 in child.
+  std::cerr << "Enabling interrupt for source 1 in child\n";
+
+  // 1.1. Determine address corresponding to child IE CSR.
+  uint64_t ieAddr = child->csrAddress(CsrNumber::Setienum);
+
+  // 2.2. Write the source id to the child IE CSR. In a real system this
+  //      would be a store which targets the APLIC.
+  unsigned writeSize = 4, sourceIx = 1;
+  aplic.write(ieAddr, writeSize, sourceIx);
+  // Same as: child->write(ieAddr, writeSize, sourceIx);
+
+  // 2. Make source1 target hart 0 with priority 1.
+
+  // 2.1. Construct value for Target1 CSR. Value encodes hart and priority.
   Target tgt{0};
   tgt.bits_.hart_ = hart;
   tgt.bits_.prio_ = 1;
-  child->write(child->csrAddress(CsrNumber::Target1), sizeof(CsrValue), tgt.value_);
+  CsrValue val = tgt.value_;
 
-  // Enable idelivery in IDC of hart 0.
-  child->write(child->ideliveryAddress(hart), sizeof(CsrValue), true);
+  // 2.2. Determine address of CSR corresponding to source 1.
+  uint64_t tgt1Addr = child->csrAddress(CsrNumber::Target1);
 
-  // Set ithreshold in IDC of hart 0.
-  child->write(child->ithresholdAddress(hart), sizeof(CsrValue), threshold);
+  // 2.3. Write CSR. In real system this would be a store.
+  aplic.write(tgt1Addr, sizeof(CsrValue), val);
+  // Same as: child->write(tg1Addr, sizeof(CsrValue), val1);
 
-  // Enable inetrrupt for source 2.
-  sourceIx = 2, hart = 1, threshold = 2;
-  root->write(root->csrAddress(CsrNumber::Setienum), writeSize, sourceIx);
+  // 3. Enable idelivery in IDC of hart 0.
+  aplic.write(child->ideliveryAddress(hart), sizeof(CsrValue), true);
+  // Same as: child->write(child->ideliveryAddress(hart), sizeof(CsrValue), true);
 
-  // Make source2 target hart 1 with effective interrupt id 7.
-  tgt.bits_.hart_ = hart;
+  // 4. Set ithreshold in IDC of hart 0.
+  CsrValue threshold = 2;
+  aplic.write(child->ithresholdAddress(hart), sizeof(CsrValue), threshold);
+
+  // 5. Enable inetrrupt for source 2 in root.
+  std::cerr << "Enabling interrupt for source 2 in root\n";
+  sourceIx = 2;
+  aplic.write(root->csrAddress(CsrNumber::Setienum), writeSize, sourceIx);
+
+  // 6. Make source2 target hart 1 with effective interrupt id 7.
+  tgt.bits_.hart_ = 1;
   tgt.mbits_.eiid_ = 7;
-
   CsrNumber tgtCsr = Domain::advance(CsrNumber::Target1, 1);
-  root->write(root->csrAddress(tgtCsr), sizeof(CsrValue), tgt.value_);
+  uint64_t tgt2Addr = root->csrAddress(tgtCsr);
+  aplic.write(tgt2Addr, writeSize, tgt.value_);
+  // Smae as: root->write(tgt2Addr, sizeof(CsrValue), tgt.value_);
 
-  // Enable idelivery in IDC of hart 1.
-  root->write(root->ideliveryAddress(hart), sizeof(CsrValue), true);
+  // 7. Enable idelivery in IDC of hart 1 in root.
+  aplic.write(root->ideliveryAddress(hart), sizeof(CsrValue), true);
 
-  // Set ithreshold in IDC of hart 1.
-  root->write(root->ithresholdAddress(hart), sizeof(CsrValue), threshold);
+  // 8. Set interrupt threshold in IDC of hart 1.
+  hart = 1; threshold = 2;
+  aplic.write(root->ithresholdAddress(hart), sizeof(CsrValue), threshold);
 
+  // 9. Chnage the state of source 3.
   aplic.setSourceState(3, true);
 
-  // Enable interrupt for source 3
-  unsigned guest = 1;
-  sourceIx = 3, hart = 1, threshold = 3;
+  // 10. Enable interrupt for source 3
+  std::cerr << "Enabling interrupt for source 3 in child 3\n";
+  sourceIx = 3;
   child3->write(child3->csrAddress(CsrNumber::Setienum), writeSize, sourceIx);
 
-  // Make source3 target hart 1, guest 1 with effective interrupt id 8.
-  tgt.bits_.hart_ = hart;
+  // 11. Make source3 target hart 1, guest 1 with effective interrupt id 8.
+  tgt.bits_.hart_ = 1;
   tgt.mbits_.eiid_ = 8;
-  tgt.mbits_.guest_ = guest;
+  tgt.mbits_.guest_ = 1;
 
-  tgtCsr = Domain::advance(CsrNumber::Target1, 2);
-  child3->write(child3->csrAddress(tgtCsr), sizeof(CsrValue), tgt.value_);
+  auto tgtCsr3 = Domain::advance(CsrNumber::Target1, 2);
+  aplic.write(child3->csrAddress(tgtCsr3), sizeof(CsrValue), tgt.value_);
 
-  // Enable idelivery in IDC of hart 1.
-  child3->write(child3->ideliveryAddress(hart), sizeof(CsrValue), true);
+  // 12. Enable idelivery in IDC of hart 1.
+  aplic.write(child3->ideliveryAddress(hart), sizeof(CsrValue), true);
 
-  // Set ithreshold in IDC of hart 1.
-  child3->write(child3->ithresholdAddress(hart), sizeof(CsrValue), threshold);
+  // 13. Set interrupt threshold in IDC of hart 1.
+  threshold = 3;
+  aplic.write(child3->ithresholdAddress(hart), sizeof(CsrValue), threshold);
 
   // Set the 1st source state to high.
+  std::cerr << "Source 1 high\n";
   aplic.setSourceState(1, true);
 
   // Set the 1st source state to low.
+  std::cerr << "Source 1 low\n";
   aplic.setSourceState(1, false);
 
   // Set the 2nd source state to high.
+  std::cerr << "Source 2 high\n";
   aplic.setSourceState(2, true);
 
   // Set the 2nd source state to low.
+  std::cerr << "Source 2 low\n";
   aplic.setSourceState(2, false);
 
   // Set the 2nd source state to high.
+  std::cerr << "Source 2 high\n";
   aplic.setSourceState(2, true);
 
   // Set the 3rd source state to low.
+  std::cerr << "Source 3 low\n";
   aplic.setSourceState(3, false);
 
   // Set the 3rd source state to low.
+  std::cerr << "Source 3 low\n";
   aplic.setSourceState(3, false);
 
   // Set the 3rd source state to high.
+  std::cerr << "Source 3 high\n";
   aplic.setSourceState(3, true);
 
   return 0;
